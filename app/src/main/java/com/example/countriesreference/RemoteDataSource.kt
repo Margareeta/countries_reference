99b3610ee0d2497c8d16ded63401032b35ea451a
Daisy <la.reine.m@gmai.com>
2022-10-19 07:30:21 +0300

    Added retrofit libs wrote RemoteDataSource.kt

---
+++ app/src/main/java/com/example/countriesreference/RemoteDataSource.kt
@@ -0,0 +1,21 @@
+package com.example.countriesreference
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.http.GET
+import retrofit2.http.Path
+
+
+class RemoteDataSource {
+    interface RestCountriesService {
+        @GET("/name/{name}")
+        fun getCountryByName(@Path("name") cityName: String): List<Country>
+    }
+
+    var retrofit = Retrofit.Builder()
+        .baseUrl("https://restcountries.com/v3.1/")
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    var restCountriesService = retrofit.create(RestCountriesService::class.java)
+}
