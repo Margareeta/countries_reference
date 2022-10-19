62b901e553e9a9956d86c0858e3e5398d56d8775
Daisy <la.reine.m@gmai.com>
2022-10-19 06:48:06 +0300

    Screen model written, binding on

---
+++ app/src/main/java/com/example/countriesreference/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.countriesreference
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.countriesreference.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
