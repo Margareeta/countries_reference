8aa1354174ac80855cee30cdc26185dece9d6d65
Daisy <la.reine.m@gmai.com>
2022-12-08 22:10:24 +0300

    final version

--- app/src/main/java/com/example/countriesreference/MainActivity.kt
+++ app/src/main/java/com/example/countriesreference/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.countriesreference
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.lifecycle.lifecycleScope
 import com.example.countriesreference.databinding.ActivityMainBinding
 import kotlinx.coroutines.launch
@@ -18,15 +19,27 @@ class MainActivity : AppCompatActivity() {
             val countryName: String = binding.countryNameEditText.text.toString()
 
             lifecycleScope.launch {
-                val counties: List<Country> = restCountriesApi.getCountryByName(countryName)
+                try{
+                    val counties: List<Country> = restCountriesApi.getCountryByName(countryName)
+
                 val country: Country = counties[0]
 
                 binding.countryNameTextView.text = country.name
                 binding.capitalNameTextView.text = country.capital
-                binding.populationNumberTextView.text = country.population.toString()
-                binding.areaNumberTextView.text = country.area.toString()
-                binding.languagesNamesTextView.text = country.languages.toString()
+                binding.populationNumberTextView.text = formantNumber(country.population)
+                binding.areaNumberTextView.text = formantNumber(country.area)
+                binding.languagesListTextView.text = langugesToString(country.languages)
 
+                loadSvg(binding.imageView, country.flag)
+                binding.resultLayout.visibility = View.VISIBLE
+                binding.resultLayout.visibility = View.INVISIBLE
+                }catch (e: Exception){
+                    binding.statusTextView.text = "Country not found"
+                    binding.statusImageFiew.setImageResource(R.drawable.ic_baseline_error_24)
+                    binding.resultLayout.visibility = View.INVISIBLE
+                    binding.resultLayout.visibility = View.VISIBLE
+
+                }
             }
         }
     }
